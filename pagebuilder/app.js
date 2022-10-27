a5aa65c0d0efe68d70000a9f030a19494181c5ad
utkukiziltas <85832700+utkukiziltas@users.noreply.github.com>
2022-10-27 22:36:36 +0300

    newups

--- pagebuilder/app.js
+++ pagebuilder/app.js
@@ -5,11 +5,63 @@ const things =
         {'id':2,'type':'editor','title':'bbbbb','content':'merhaba','col':12},
         {'id':3,'type':'editor','title':'ccccc','content':'merhaba','col':12},
     ]
+
 const thing = (value) => {
-  return `<div id="thing-${value.id}" class="${value.type} col-${value.col}">${value.content}</div>`
+  return `<div id="thing-${value.id}" class="${value.type}  col-${value.col} thing" draggable="true">${value.title}</div>`
 }
 
-let root = document.querySelector('#page-builder')
+let root = document.querySelector('#page-builder'),
+    row = root.querySelector('.row')
+
+
+
 things.forEach((value)=>{
-    $(root).append(thing(value))
+    $(row).append(thing(value))
 })
+
+
+
+
+setTimeout(function (){root.querySelectorAll("div").forEach((thing) => {
+
+
+
+    thing.addEventListener('dragstart', (e) => {
+        e.target.setAttribute('dragging',true)
+        e.target.classList.add('bg-danger')
+
+    });
+
+    thing.addEventListener('drop', (e) => {
+        e.target.classList.remove('bg-danger')
+        let dragingthing = row.querySelector('[dragging]')
+
+        e.target.before(dragingthing)
+        console.log(things)
+    });
+
+    thing.addEventListener('dragenter', (e) => {
+        e.target.classList.add('bg-danger')
+        e.preventDefault();
+    });
+
+    thing.addEventListener('dragover', (e) => { e.preventDefault()});
+
+    thing.addEventListener('dragleave', (e) => {
+
+        e.target.classList.remove('bg-danger')
+
+    });
+
+
+
+    thing.addEventListener('dragend', (e) => {
+        e.target.removeAttribute('dragging')
+    });
+
+
+
+
+
+
+})},10)
