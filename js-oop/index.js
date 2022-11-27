b8937b177015813bae3187035ccd17d5c1ad5444
utkukiziltas <85832700+utkukiziltas@users.noreply.github.com>
2022-11-27 17:30:54 +0300

    javascript

---
+++ js-oop/index.js
@@ -0,0 +1,34 @@
+
+
+/*
+function createCircle(radius,){
+
+    return {
+        radius:radius,//sadece radius yazsanda çalışır
+       draw:function () {
+           console.log("draw")
+       }
+    }
+}
+
+const circle = createCircle(1)
+circle.draw()
+
+
+
+//constructor functon
+
+function Circle(radius){
+    this.radius = radius;
+    this.draw=function () {
+        console.log("draw")
+    }
+}
+const another = new Circle(1);
+
+
+//
+let x = {}
+// let x = new Object()
+
+ */
